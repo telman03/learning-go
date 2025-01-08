8c56c721f77a3f6fae1e1db8e380445462f921f5
telman03 <telmangadimov1@gmail.com>
2025-01-08 19:08:52 +0400

    Added new task type assertions

---
+++ type_assertions_in_go/main.go
@@ -0,0 +1,51 @@
+package type_assertions_in_go
+
+func getExpenseReport(e expense) (string, float64) {
+	em, ok := e.(email)
+	if ok {
+		return em.toAddress, em.cost()
+	}
+	s, ok := e.(sms)
+	if ok {
+		return s.toPhoneNumber, s.cost()
+	}
+	return "", 0.0
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
---
+++ type_assertions_in_go/readme.md
@@ -0,0 +1,7 @@
+### Assignment
+
+Implement the getExpenseReport function.
+
+If the **expense** is an *email* then it should return the email's *toAddress* and the *cost* of the email.
+If the *expense* is an *sms* then it should return the sms's *toPhoneNumber* and its cost.
+If the expense has any other underlying type, just return an empty string and *0.0* for the cost.
