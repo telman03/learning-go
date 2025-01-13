d1cc0069d8a6c32ae61dcd5e8d64b6bd74c3efe8
telman03 <telmangadimov1@gmail.com>
2025-01-13 17:21:21 +0400

    Added new task "loops"

---
+++ loops_in_go/main.go
@@ -0,0 +1,9 @@
+package loops_in_go
+
+func bulkSend(numMessages int) float64 {
+	totalCost := 0.0
+	for i := 0; i < numMessages; i++ {
+		totalCost += 1.0 + (0.01 * float64(i))
+	}
+	return totalCost
+}
